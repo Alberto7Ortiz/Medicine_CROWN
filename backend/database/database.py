7d30f0a99c605889bef860b67f278fb9b50ed7c9
Alberto Ortiz <faoc1992@gmail.com>
2026-07-27 14:53:37 -0400

    Rewrite database connection setup

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,19 +1,37 @@
 from pathlib import Path
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import (
+    sessionmaker,
+    declarative_base
+)
 
+
+
+# ==================================
+# Database path
+# ==================================
 
 BASE_DIR = Path(__file__).resolve().parent
 
-DB_PATH = BASE_DIR / "station.db"
+DB_PATH = BASE_DIR / "medicion_crown.db"
 
+
+
+# ==================================
+# Engine
+# ==================================
 
 engine = create_engine(
     f"sqlite:///{DB_PATH}",
     echo=False
 )
 
+
+
+# ==================================
+# Session
+# ==================================
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -22,8 +40,19 @@ SessionLocal = sessionmaker(
 )
 
 
+
+# ==================================
+# Base class
+# ==================================
+
 Base = declarative_base()
 
 
+
+# ==================================
+# Session generator
+# ==================================
+
 def get_session():
+
     return SessionLocal()
